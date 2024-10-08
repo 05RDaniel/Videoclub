00fa87c81b3efef13d49c1f34b6f829c52423dc5
Desock <salvagomezrisen@gmail.com>
2024-10-08 16:22:10 +0200

    test de parte salva

---
+++ Parte_Salva/Soporte.php
@@ -0,0 +1,12 @@
+<?php
+    class Soporte {
+        public $titulo;
+        protected $numero;
+        private $precio;
+
+        public function __construct($t, $n, $p)
+        {
+            
+        }
+    }
+?>
