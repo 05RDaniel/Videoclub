076930c0a4dba2c50ceb28883787c2c6bd5ab47e
Desock <salvagomezrisen@gmail.com>
2024-10-08 17:04:17 +0200

    Clase soporte

--- Parte_Salva/Soporte.php
+++ Parte_Salva/Soporte.php
@@ -3,10 +3,39 @@
         public $titulo;
         protected $numero;
         private $precio;
+        private static const IVA = 0.21;
 
         public function __construct($t, $n, $p)
         {
-            
+            $this->titulo = $t;
+            $this->numero = $n;
+            $this->precio = $p;  
+        }
+
+        public function getPrecio($name)
+        {
+            if ($name == 'pre') {
+                return $this->precio;
+            }
+        }
+
+        public function getNumero($name) 
+        {
+            if ($name == 'num') {
+                return $this->numero;
+            }
+        }
+
+        public function getPrecioIVA() 
+        {
+            $precio_iva = $this->precio * self::IVA;
+            $precio_iva_total = $this->precio + $precio_iva;
+            echo $precio_iva_total;
+        }
+
+        public function muestraResumen()
+        {
+            echo "<p>Titulo: $this->titulo<br>" . "Numero: $this->numero<br>" . "Precio: $this->precio<br>" . "Precio con iva: </p>";
         }
     }
 ?>
