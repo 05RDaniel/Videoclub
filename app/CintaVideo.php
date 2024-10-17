9c200ae305e56c74b838cf1ff5830d3944eedef0
Desock <salvagomezrisen@gmail.com>
2024-10-17 17:52:30 +0200

    Version final de la aplicacionvideoclub

--- app/CintaVideo.php
+++ app/CintaVideo.php
@@ -1,2 +1,17 @@
 <?php
+    Class CintaVideo extends Soporte
+    {
+        public function __construct($t, $p, private $duracion)
+        {
+            parent::__construct($t, $p);
+            parent::getCodigoProducto();
+        }
+
+        public function muestraResumen()
+        {
+            parent::muestraResumen();
+            echo "<p>Duracion: " . $this->duracion . "</p>";
+        }
+
+    }
 ?>
