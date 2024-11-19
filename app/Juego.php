b37bebc198ffc879a40036b11087842a033521d5
Daniel <daniel.rufes@gmail.com>
2024-11-19 16:08:38 +0100

    Corrigiendo errores

--- app/Juego.php
+++ app/Juego.php
@@ -1,6 +1,5 @@
 <?php
     namespace Videoclub\app;
-    include_once "Soporte.php";
     Class Juego extends Soporte
     {
         public function __construct($t, $p, public $console, private $minNumJugadores, private $maxNumJugadores)
@@ -11,21 +10,21 @@
 
         //Metodo que comprueba el numero de jugadores que pueden jugar a los juegos
         public function muestraJugadoresPosibles() {
-            if ($this->minNumJugadores == 1 && $this->maxNumJugadores == 0) {
-                echo "El juego es para 1 jugador.";
+            if ($this->minNumJugadores == 1 && $this->maxNumJugadores == 1) {
+                echo "<br>El juego es para 1 jugador.";
             } else if ($this->minNumJugadores == 0 && $this->maxNumJugadores > 1) {
-                echo "El juego es para " . $this->maxNumJugadores . " jugadores.";
+                echo "<br>El juego es para " . $this->maxNumJugadores . " jugadores.";
             } else if ($this->minNumJugadores != 0 && $this->maxNumJugadores != 0) {
-                echo "El juego es de " . $this->minNumJugadores . " a " . $this->maxNumJugadores . " jugadores.";
+                echo "<br>El juego es de " . $this->minNumJugadores . " a " . $this->maxNumJugadores . " jugadores.";
             } else {
-                echo "El juego no es valido.";
+                echo "<br>El juego no es valido.";
             }
         }
 
         public function muestraResumen()
         {
             parent::muestraResumen();
-            echo "<br>Tipo de consola: " . $this->console . "<br>" . $this->muestraJugadoresPosibles()."<br>";
+            echo "<br>Tipo de consola: " . $this->console . "<br>" . $this->muestraJugadoresPosibles();
         }
     }
 ?>
